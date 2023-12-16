b048e3aaba8ed93d3a55835654911bb629783650
MatDev <gaunam134@gmail.com>
2023-12-16 17:39:08 -0300

    first changes

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google'
+import { Ubuntu } from 'next/font/google'
+import { Lusitana } from 'next/font/google'
+
+export const inter = Inter({subsets:['latin']})
+export const lusitana = Lusitana({subsets:['latin'], weight:['400']})
+export const ubuntu = Ubuntu({subsets:['latin'], weight:['300']})
